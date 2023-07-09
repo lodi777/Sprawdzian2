7b26345ed1a59217d441d84abe2f5b8d44887ddf
lodi777 <122987254+lodi777@users.noreply.github.com>
2023-07-09 13:25:17 +0200

    Zadanie 1 podpunkt b

--- src/main/java/pl/kurs/app/Main.java
+++ src/main/java/pl/kurs/app/Main.java
@@ -5,9 +5,23 @@ import pl.kurs.models.Figure;
 public class Main {
     public static void main(String[] args) {
 
-        Figure[] figures = {Figure.createSquare(10), Figure.createCircle(10), Figure.createRectangle(10, 20), Figure.createSquare(15)};
+        Figure[] figures = {Figure.createSquare(100), Figure.createCircle(10), Figure.createRectangle(10, 20), Figure.createSquare(158)};
         for (Figure f : figures) {
             System.out.println(f);
         }
+        Figure highestCircuit = figures[0];
+        Figure highestSurface = figures[0];
+
+        for (int i = 1; i < figures.length; i++) {
+            if (figures[i].circuit() > highestCircuit.circuit()) {
+                highestCircuit = figures[i];
+            }
+
+            if (figures[i].surface() > highestSurface.surface()) {
+                highestSurface = figures[i];
+            }
+        }
+        System.out.println("Figura z najwiekszym obwodem: " + highestCircuit);
+        System.out.println("Figura z najwiekszym polem: " + highestSurface);
     }
 }
